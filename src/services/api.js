9c67051dee6cd126b7bdf772797a2fc38111b203
SHUBHAM-MAKODE <SHIVAM460551@GMAIL.COM>
2026-06-06 16:28:05 +0530

    new assets

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL:
-    "https://shubham-portfolio-api-3i28.onrender.com/api" ||
-    "http://localhost:8081/api",
+  baseURL: "https://shubham-portfolio-api-3i28.onrender.com/api"||"http://localhost:8081/api",
   headers: {
     "Content-Type": "application/json",
   },
